682faa78d9a41408db9caa9ccf107aac0f4af3c2
D3lta01 <israel19962012@gmail.com>
2021-09-12 16:57:15 -0400

    Basic static Components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+function Footer () {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.instagram.com/_israel_filho/" target="_blank" rel="noreferrer">Israel Filho</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
