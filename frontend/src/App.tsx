4aa5d2e056b08278c949a01ad64e6e126e715058
D3lta01 <israel19962012@gmail.com>
2021-09-12 20:49:43 -0400

    update charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,30 +9,25 @@ function App() {
       <NavBar />
       <div className="container">
         <h1 className="text-primary py-3">Painel de Vendas</h1>
-      
-        <div className= "row px-3">
-         <div className="col-sm-6">
-           <h5 className="Text center text-secondary" >Taxa de Sucesso %</h5>
-           <BarChart />
-           
-           <div className="col-sm-6">
-           <h5 className="Text center text-secondary" >Total de Vendas</h5>
-           <DonutChart />
-          </div>          
-             </div>  
-                           
-        </div>   
-         
-         <div className= "py-3">
-           <h2 className="text-primary"> Todas Vendas</h2>
-         </div>
-    
 
+        <div className="row px-3">
+          <div className="col-md-6">
+            <h5 className="Text center text-secondary" >Taxa de Sucesso %</h5>
+            <BarChart />
+          </div>
+          <div className="col-md-6">
+            <h5 className="Text center text-secondary" >Taxa de Sucesso %</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary"> Todas Vendas</h2>
+        </div>
         <DataTable />
-
       </div>
       <Footer />
-    
     </>
   );
 }
